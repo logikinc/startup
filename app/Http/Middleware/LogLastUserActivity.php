0ac235fa98adec7053de087d1214c16a1e1f6ddf
Kjetil Elde <kjetil@stiligt.no>
2016-09-11 18:51:41 +0000

    Applied fixes from StyleCI

--- app/Http/Middleware/LogLastUserActivity.php
+++ app/Http/Middleware/LogLastUserActivity.php
@@ -2,26 +2,28 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
 use Auth;
-use Carbon\Carbon;
 use Cache;
+use Carbon\Carbon;
+use Closure;
 
 class LogLastUserActivity
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        if(Auth::check()) {
+        if (Auth::check()) {
             $expiresAt = Carbon::now()->addMinutes(5);
-            Cache::put('user-is-online-' . Auth::user()->id, true, $expiresAt);
+            Cache::put('user-is-online-'.Auth::user()->id, true, $expiresAt);
         }
-        return $next($request);    
+
+        return $next($request);
     }
 }
