bb1fa9623f2c64e37cc682058f36b1b13d67a7a4
Kjetil Elde <kjetil@stiligt.no>
2016-09-13 05:49:37 +0000

    Applied fixes from StyleCI

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -2,30 +2,28 @@
 
 namespace App\Http\Middleware;
 
+use App;
 use Closure;
 use Session;
-use App;
-use Config;
 
 class SetLocale
 {
-    
-     protected $languages = ['en','no'];
-    
+    protected $languages = ['en', 'no'];
+
     /**
-     *
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
      * @return mixed
      */
-	public function handle($request, Closure $next)
-	{
-        if(Session::has('locale') && in_array(Session::get('locale'), $this->languages))
-        {
+    public function handle($request, Closure $next)
+    {
+        if (Session::has('locale') && in_array(Session::get('locale'), $this->languages)) {
             App::setLocale(Session::get('locale'));
         }
-		return $next($request);
-	}
+
+        return $next($request);
+    }
 }
