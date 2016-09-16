0da66b8ffbbddb840894954578c5e3b1cfa5e871
w00p <kjetil@stiligt.no>
2016-09-16 08:54:23 +0200

    Updated .env.example and mail from name and address

--- config/mail.php
+++ config/mail.php
@@ -57,8 +57,8 @@ return [
     */
 
     'from' => [
-        'address' => 'hello@example.com',
-        'name'    => 'Example',
+        'address' => env('MAIL_FROM',   'hello@example.com'),
+        'name'    => env('MAIL_FROM_NAME',   'Example')
     ],
 
     /*
